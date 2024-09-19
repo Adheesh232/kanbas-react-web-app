22aeb92f635247d422c7a7fa5dddb1ea2ada890d
Adheesh <adheeshsreedharan02@gmail.com>
2024-09-19 16:04:52 -0400

    a1 branch commit

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,8 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><a id="wd-github" href="https://github.com/Adheesh232/kanbas-react-web-app/tree/a1"> Github Link</a></li> 
+
       </ul>
     );
   }
